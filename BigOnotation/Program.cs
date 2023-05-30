ac28b446fe457a77bc2c8a3908708c0eea09e448
mohammad alqutami <mohammedalqutami@yahoo.com>
2023-05-30 20:15:36 +0300

    first commit

---
+++ BigOnotation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+int[] x = new int[10];
+for(int i=0;i<x.Length;i++) //o(n)
+{
+    x[i] =x[i] *5; //o(n)
+}
+
+int[] y = new int[10];
+for(int i=0;i<y.Length;i++) //o(n)
+{
+    y[i] =y[i] *5; //o(1)
+}
+ for(int i=0;i<x.Length;i++) //o(n)
+ {
+if(x[i]==70) //o(1)
+{
+    for(int k=0;i<y.Length;k++)//o(n)
+    {
+if(y[k]==x[i])//o(1)
+{
+Console.WriteLine("the index of 70 in y is"+k);//o(n)
+{
+    
+}
+}
+    }
+}
+ } ///time complixty = o(n^2)      // n *n = n^
